5c981221400f6cd54a0444b4642537ddd329ed20
Apex04 <ajbobay@mailfence.com>
2025-11-13 09:00:23 -0500

    began framing pid.rs

--- src/eclipselib/pid.rs
+++ src/eclipselib/pid.rs
@@ -4,7 +4,7 @@
 /*    File: pid.rs                                  */
 /*    Author: Andrew Bobay                          */
 /*    Date Created: Nov 9th 2025 11:30AM            */
-/*    Date Modified: Nov 9th 2025 11:30PM           */
+/*    Date Modified: Nov 13th 2025 8:00AM           */
 /*    Team: BBR1                                    */
 /*    Description: pid functions                    */
 /*                                                  */
